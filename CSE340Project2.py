c282e0c334242015dc3e1c1b35f612edad1fd391
Sai Pc <schand31@asu.edu>
2015-09-20 12:50:53 -0700

    Added a file separator script to separate files that need correction

--- CSE340Project2.py
+++ CSE340Project2.py
@@ -1,6 +1,32 @@
+import os
+import shutil
 __author__ = 'saipc'
 
-def checkForMemoryAlloc():
+def separateFilesToBeCorrected(dirName):
     # do nothing now
-    x = []
+    os.chdir(dirName)
+    folders = []
+    correctionList = r"/Users/schandramouli/Documents/correctionList"
+    with open(correctionList) as file:
+            folders = file.readlines()
+    print folders
 
+    folderList=os.listdir(dirName)
+    for folderName in folderList:
+        formattedFolderName = str(folderName) + "\n"
+        if formattedFolderName in folders:
+            print folderName
+            copyDirectory(folderName, os.path.join(dirName, r"correction-saipc", folderName))
+
+def copyDirectory(src, dest):
+    try:
+        shutil.copytree(src, dest)
+    # Directories are the same
+    except shutil.Error as e:
+        print('Directory not copied. Error: %s' % e)
+    # Any error saying that the directory doesn't exist
+    except OSError as e:
+        print('Directory not copied. Error: %s' % e)
+
+
+separateFilesToBeCorrected(r"/Users/schandramouli/Documents/CSE 340 Project 2/cse340_f15_p2_submissions")
