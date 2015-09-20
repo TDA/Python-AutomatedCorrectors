ead69407d9fe43a0a00609a83dc32d9bf9781050
Sai Pc <schand31@asu.edu>
2015-09-20 13:48:24 -0700

    added lexer checks

---
+++ AutoCorrect.py
@@ -0,0 +1,31 @@
+import os
+import re
+__author__ = 'saipc'
+
+def checkForLexer(dirName):
+    os.chdir(dirName)
+    folderList=os.listdir(dirName)
+    folderList.pop(0)
+    errorRE = re.compile(r"ERROR")
+    ungetTokenRE = re.compile(r"ungetToken\(\)")
+    #print folderList
+    for folderName in folderList:
+        files = os.listdir(folderName)
+        if '_test_results_' in files:
+            files.remove('_test_results_')
+        if 'lexer.c' in files:
+            files.remove('lexer.c')
+        if 'lexer.h' in files:
+            files.remove('lexer.h')
+        #print files
+        if len(files) == 1:
+            with open(os.path.join(folderName, files.pop()), 'r') as file:
+                data = file.read().replace('\n', '')
+                if 'ERROR' in data:
+                    print folderName, "error"
+                if r"unget" in data:
+                    print folderName, "unget"
+
+
+
+checkForLexer(r"/Users/schandramouli/Documents/CSE 340 Project 2/cse340_f15_p2_submissions/correction-saipc")
