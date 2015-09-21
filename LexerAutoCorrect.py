aeb530df50b52245dd0696a5306b6f45234bbfd1
Sai Pc <schand31@asu.edu>
2015-09-21 13:40:50 -0700

    changed to iterate over all student created files

--- LexerAutoCorrect.py
+++ LexerAutoCorrect.py
@@ -18,14 +18,13 @@ def checkForLexer(dirName):
         if 'lexer.h' in files:
             files.remove('lexer.h')
         #print files
-        if len(files) == 1:
-            with open(os.path.join(folderName, files.pop()), 'r') as file:
+        for fileName in files:
+            with open(os.path.join(folderName, fileName), 'r') as file:
                 data = file.read().replace('\n', '')
                 if 'ERROR' in data:
                     print folderName, "error"
                 if 'unget' in data:
                     print folderName, "unget"
-
 
 
 checkForLexer(r"/Users/schandramouli/Documents/CSE 340 Project 2/cse340_f15_p2_submissions/correction-saipc")
